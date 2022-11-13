82f35759853d105ffe63df6bb1c6e696665a3475
rezapfebriyan <rezafebriyan00@gmail.com>
2022-11-13 12:00:46 +0700

    add controller change password

---
+++ app/Http/Controllers/UpdatePasswordController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UpdatePasswordController extends Controller
+{
+    public function edit()
+    {
+        return view('password.edit');
+    }
+
+    public function update(Request $request)
+    {
+        $attr = $request->validate([
+            'current_password' => ['required'],
+            'password' => ['min:8', 'confirmed']
+        ]);
+
+        if (Hash::check($request->current_password, auth()->user()->password)) {
+            auth()->user()->update(['password' => Hash::make($request->password)]);
+            return back()->with('message', 'Your password has been changed');
+        }
+
+        throw ValidationException::withMessages([
+            'current_password' => "Your current password does not match with our record"
+        ]);
+    }
+}
